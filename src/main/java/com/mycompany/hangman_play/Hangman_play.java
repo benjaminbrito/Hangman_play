663482473493c39eef351979db987e95364dffdc
Benjamin <benjaminalexbrito@gmail.com>
2023-04-27 20:31:05 -0600

    se inicializa el juego de hangman-ahorcado-

---
+++ src/main/java/com/mycompany/hangman_play/Hangman_play.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hangman_play;
+
+/**
+ *
+ * @author Benjamin
+ */
+public class Hangman_play {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(" ____________\n|            |\n|            |\n|\n|\n|\n|\n|              ");
+        
+    }
+}
---
+++ src/main/java/com/mycompany/hangman_play/controladores/pintadores.java
@@ -0,0 +1,10 @@
+package com.mycompany.hangman_play.controladores;
+
+public class pintadores {
+
+    public void pintarCara() {
+        System.out.println("\n|            o");
+        
+    }
+    
+}
